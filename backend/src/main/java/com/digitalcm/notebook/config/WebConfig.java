a45141c578b2dcf2f7a401ae10bbf01b8563e73b
helldiving <142443321+helldiving@users.noreply.github.com>
2024-11-15 15:42:59 -0500

    Fixed CORS configuration
    Vercel domain was being blocked.

--- backend/src/main/java/com/digitalcm/notebook/config/WebConfig.java
+++ backend/src/main/java/com/digitalcm/notebook/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://notebook-digital-cm.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
